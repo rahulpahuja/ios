aac19888ffc0ed2339f218813957211f669b6c32
RP <rahulpahuja2015@gmail.com>
2020-12-26 16:00:08 +0530

    Added Initial Project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rahul Pahuja on 26/12/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
